56bdc2abe4348f536042e73ec76213e736ea21a9
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-01-07 01:55:08 -0300

    Api
    Colocado uma api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import Api from "../../services/api";
+
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() =>{
+    async function loadFilmes(){
+      const response = await Api.get('r-api/?api=filmes')
+      //console.log(response.data);
+      setFilmes(response.data);
+    }
+
+    loadFilmes();
+
+  }, []);
+
     return (
-      <div>
-        <h1>Pagina Home</h1>
+      <div className="container">
+        <div className="lista-filmes">
+          {filmes.map((filme) =>{
+            return (
+              <article key={filme.id}>
+                <strong> {filme.id} </strong>
+              </article>
+            )
+          })}
+        </div>
       </div>
     )
   }
